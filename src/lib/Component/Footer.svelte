cafd0de4afb54308b2f744e11e92f71d93ba4b5c
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-05-21 13:07:37 +0530

    ☄️ add footer

---
+++ src/lib/Component/Footer.svelte
@@ -0,0 +1,8 @@
+<footer class="mx-auto shadow flex bg-gray-800">
+  <a href="https://github.com/Az-21/" class="text-gray-500 hover:underline hover:text-emerald-500">Made by Az-21</a>
+
+  <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400">
+    <li><a href="#" class="mr-4 hover:underline ">Report Bug</a></li>
+    <li><a href="https://shields.io/" class="mr-4 hover:underline">Shields.io</a></li>
+  </ul>
+</footer>
