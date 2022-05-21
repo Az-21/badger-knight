ad0048da4698e9a269cd3f4e0e08f40336145f97
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-05-21 13:19:00 +0530

    💚 improve footer style

--- src/lib/Component/Footer.svelte
+++ src/lib/Component/Footer.svelte
@@ -1,8 +1,12 @@
-<footer class="mx-auto flex bg-gray-800 shadow">
-  <a href="https://github.com/Az-21/" class="text-gray-500 hover:text-emerald-500 hover:underline">Made by Az-21</a>
+<footer class="mx-auto flex justify-between bg-zinc-800 p-12">
+  <a href="https://github.com/Az-21/" class="text-gray-200 hover:text-emerald-500 hover:underline">💚 Made by Az-21</a>
 
-  <ul class="mt-3 flex flex-wrap items-center text-sm text-gray-500 dark:text-gray-400">
-    <li><a href="#" class="mr-4 hover:underline ">Report Bug</a></li>
-    <li><a href="https://shields.io/" class="mr-4 hover:underline">Shields.io</a></li>
+  <ul class="flex flex-wrap items-center text-gray-400 dark:text-gray-400">
+    <li>
+      <a href="#" class="mr-4 hover:underline">🪲 Report Bug</a>
+    </li>
+    <li>
+      <a href="https://shields.io/" class="mr-4 font-bold text-white hover:underline">🛡️ Shields.io</a>
+    </li>
   </ul>
 </footer>
