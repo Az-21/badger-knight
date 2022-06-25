1c87c6bf054f116e7b142d024bf2764fb2d3458b
Az-21 <39803478+Az-21@users.noreply.github.com>
2022-06-25 21:36:53 +0530

    🎴 import Daisy UI library

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,7 @@
 const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-
-  theme: {
-    extend: {}
-  },
-
-  plugins: []
+  theme: { extend: {} },
+  plugins: [require("daisyui")],
 };
 
 module.exports = config;
